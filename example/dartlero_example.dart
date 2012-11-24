763e78d350bd4ebdb55fe03ed5812f583015da5d
Dzenan Ridjanovic <dzenanr@gmail.com>
2012-11-23 19:52:49 -0500

    update example

---
+++ example/dartlero_example.dart
@@ -0,0 +1,7 @@
+library dartlero_example;
+
+import 'package:dartlero/dartlero.dart';
+import 'package:unittest/unittest.dart';
+
+part 'project_model/project_entities.dart';
+part 'project_model/project_model.dart';
